c298372f3430bed49eefc5d52fbd2612dd760b55
Arvin M <arvinmefsha1@gmail.com>
2025-09-21 18:45:41 -0400

    release

--- game.py
+++ game.py
@@ -1,22 +1,15 @@
-# game.py
-
 class Game:
     def update(self, pose_data, dt):
-        """Update game state based on pose data and time delta."""
         pass
     
     def render(self, frame):
-        """Render game visuals on the frame."""
         pass
     
     def handle_input(self, pose_data):
-        """Process pose data for game logic."""
         pass
     
     def reset(self):
-        """Reset game state."""
         pass
     
     def is_menu(self):
-        """Return True if this is a menu, False if a playable game."""
         return False
